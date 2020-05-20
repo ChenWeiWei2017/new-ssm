b328fcc2503aef14fc338fa24ade6fdffe43da77
chenweiwei <1752083854@qq.com>
2020-05-20 13:43:22 +0800

    初始化

---
+++ src/main/java/xyz/chenww/newssm/NewSsmApplication.java
@@ -0,0 +1,13 @@
+package xyz.chenww.newssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSsmApplication.class, args);
+    }
+
+}
